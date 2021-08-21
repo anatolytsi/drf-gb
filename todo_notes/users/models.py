f2561d6550a078def6a75ad1f626c3c1395f8618
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-21 11:41:32 +0200

    lesson1: added a more verbose string representation of User model instance

--- todo_notes/users/models.py
+++ todo_notes/users/models.py
@@ -7,3 +7,6 @@ class User(AbstractUser):
     A standard User model from an AbstractUser to inherit the default fields from the latter
     """
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.username} | {self.email} | {self.first_name} {self.last_name}'
