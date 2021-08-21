1587159c7a2771f59a0993dd0682e267b41b8aba
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-21 11:34:08 +0200

    lesson1: added superuser creation script

---
+++ todo_notes/users/management/commands/create_superuser.py
@@ -0,0 +1,45 @@
+import environ
+
+from users.models import User
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    DEFAULT_SU_LOGIN=(str, 'admin'),
+    DEFAULT_SU_PASS=(str, 'admin'),
+    DEFAULT_SU_EMAIL=(str, 'admin@admin.admin')
+)
+
+# Reading .env file
+base = environ.Path(__file__) - 4  # Environment file is situated four folders back
+environ.Env.read_env(env_file=base('.env'))
+
+
+def create_superuser(
+        username=env('DEFAULT_SU_LOGIN'),
+        password=env('DEFAULT_SU_PASS'),
+        email=env('DEFAULT_SU_EMAIL'),
+        first_name="Admin",
+        last_name="Django"):
+    """
+    Creates a superuser for Django
+    :param username: superuser username
+    :param password: superuser password
+    :param email: superuser emails
+    :param first_name: superuser first name
+    :param last_name: superuser last name
+    :return: user model instance
+    """
+
+    user = User(
+        username=username,
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+    )
+    user.set_password(password)
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+
+    return user
