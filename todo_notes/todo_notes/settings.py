0e55fd01e2941de698243187fa4c21f0a1212eb4
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-21 11:13:22 +0200

    lesson1: moved .env to other location

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -12,13 +12,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
 
 # Reading .env file
-environ.Env.read_env()
+base = environ.Path(__file__) - 2  # Environment file is situated two folders back
+environ.Env.read_env(env_file=base('.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
