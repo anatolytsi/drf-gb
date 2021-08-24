e4cf92bfcb7b6d812a91da8e6a8e68fd2998f138
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-24 21:56:30 +0200

    lesson2: added React

---
+++ todo_notes/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from "react";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = [];
+    }
+
+    render() {
+        return (
+            <div>Main App</div>
+        )
+    }
+}
+
+export default App;
