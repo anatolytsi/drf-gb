1fdf4336d6f1e4cc8807cd59fbed2c716467f0ef
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-24 22:42:00 +0200

    lesson2: implemented basic user placeholders into App

--- todo_notes/frontend/src/App.js
+++ todo_notes/frontend/src/App.js
@@ -1,17 +1,44 @@
 import './App.css';
-import React from "react";
+import React from 'react';
+import UserList from './components/User'
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = [];
+        this.state = {
+            'users': []
+        };
+    }
+
+    componentDidMount() {
+        const users = [
+            {
+                'username': 'ivan_ivanov',
+                'email': 'ivan.ivanov@mail.ru',
+                'first_name': 'Ivan',
+                'last_name': 'Ivanov'
+            },
+            {
+                'username': 'vasya_pupkin',
+                'email': 'vasya.pupkin@mail.ru',
+                'first_name': 'Vasya',
+                'last_name': 'Pupkin'
+            }
+        ];
+        this.setState(
+            {
+                users
+            }
+        );
     }
 
     render() {
         return (
-            <div>Main App</div>
-        )
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        );
     }
 }
 
