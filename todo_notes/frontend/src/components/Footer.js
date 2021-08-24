9d2a12eb7b0bd43569252817f67ae7f8c55c55ea
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-25 00:25:06 +0200

    lesson2: added footer and menu components

---
+++ todo_notes/frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Footer = () => {
+    return (
+        <footer className='footer-copyright fixed-bottom text-center bg-dark bg-opacity-25 py-3'>
+            &copy; {new Date().getFullYear()} Copyright: <a className='text-decoration-none' href='https://www.linkedin.com/in/anatolytsi'> Anatolii Tsirkunenko </a>
+        </footer>
+    )
+}
+
+export default Footer
