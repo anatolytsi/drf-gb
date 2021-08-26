805508ab3c45452e9a4de148db726c24c7d65105
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 20:47:14 +0200

    lesson3: added renderers for User model

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -144,3 +144,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- todo_notes/users/views.py
+++ todo_notes/users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User
@@ -8,5 +9,6 @@ class UserModelViewSet(ModelViewSet):
     """
     Provides actions to list, retrieve, create, update or destroy User model/-s
     """
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
