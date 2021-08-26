9b67fc60d6c3ad9abe1ddf64163a35e62c686ee0
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 21:02:00 +0200

    lesson3: added project and todo notes serializers

---
+++ todo_notes/notes/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+
+from notes.models import Project, Note
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    """
+    A serializer (i.e. convert querysets and model instances to Python datatypes or back) that corresponds to the
+    Project model field
+    HyperlinkedModelSerializer - hyperlinks are used for representation instead of primary keys (unlike ModelSerializer)
+    """
+
+    class Meta:
+        model = Project
+        fields = '__all__'  # The model is custom built so we need all the fields we put there
+
+
+class NoteModelSerializer(HyperlinkedModelSerializer):
+    """
+    A serializer (i.e. convert querysets and model instances to Python datatypes or back) that corresponds to the Note
+    model field
+    HyperlinkedModelSerializer - hyperlinks are used for representation instead of primary keys (unlike ModelSerializer)
+    """
+
+    class Meta:
+        model = Note
+        fields = '__all__'  # The model is custom built so we need all the fields we put there
