129bd2a2fb454c0e471d283fa1784124b61b0f72
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 22:39:42 +0200

    lesson3: added interactive menu

--- todo_notes/frontend/src/App.js
+++ todo_notes/frontend/src/App.js
@@ -4,6 +4,8 @@ import UserList from './components/User';
 import axios from "axios";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
+import ProjectList from './components/Project';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 const apiUrl = 'http://localhost:8000/api/';
 const getUrl = (name) => `${apiUrl}${name}`;
@@ -12,7 +14,8 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'users': []
+            'users': [],
+            'projects': []
         };
     }
 
@@ -27,15 +30,38 @@ class App extends React.Component {
                 );
             })
             .catch(error => console.error(error));
+        axios.get(getUrl('projects'))
+            .then(response => {
+                const projects = response.data;
+                this.setState(
+                    {
+                        projects
+                    }
+                );
+            })
+            .catch(error => console.error(error));
     }
 
     render() {
         return (
-            <div>
+            <Router>
                 <Menu/>
-                <UserList users={this.state.users}/>
+                <Switch>
+                    <Route
+                        path='/users'
+                        render={(props) => (
+                            <UserList {...props} users={this.state.users}/>
+                        )}
+                    />
+                    <Route
+                        path='/projects'
+                        render={(props) => (
+                            <ProjectList {...props} projects={this.state.projects}/>
+                        )}
+                    />
+                </Switch>
                 <Footer/>
-            </div>
+            </Router>
         );
     }
 }
--- todo_notes/frontend/src/components/Menu.js
+++ todo_notes/frontend/src/components/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 
 const Menu = () => {
@@ -8,10 +9,12 @@ const Menu = () => {
             <span className="ms-2 navbar-brand mb-0 h1">Menu</span>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-item nav-link active" href="#">List of all users</a>
-                    <a className="nav-item nav-link disabled" href="#">User</a>
-                    <a className="nav-item nav-link disabled" href="#">Login</a>
-                    <a className="nav-item nav-link disabled" href="#">etc.</a>
+                    <Link to='/users' className="nav-item nav-link active">
+                        List of all users
+                    </Link>
+                    <Link to='/projects' className="nav-item nav-link active">
+                        List of all projects
+                    </Link>
                 </div>
             </div>
         </nav>
