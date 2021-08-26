208f1342e66b8ba8d7d647d7ff670b826c31ddfc
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 23:04:34 +0200

    lesson3: added home and note components to menu

--- todo_notes/frontend/src/components/Menu.js
+++ todo_notes/frontend/src/components/Menu.js
@@ -6,7 +6,9 @@ import { Link } from 'react-router-dom';
 const Menu = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <span className="ms-2 navbar-brand mb-0 h1">Menu</span>
+            <Link to='/' className="ms-2 navbar-brand mb-0 h1">
+                Menu
+            </Link>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <Link to='/users' className="nav-item nav-link active">
@@ -14,6 +16,9 @@ const Menu = () => {
                     </Link>
                     <Link to='/projects' className="nav-item nav-link active">
                         List of all projects
+                    </Link>
+                    <Link to='/notes' className="nav-item nav-link active">
+                        List of all notes
                     </Link>
                 </div>
             </div>
