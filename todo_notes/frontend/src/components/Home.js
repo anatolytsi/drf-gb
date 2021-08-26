d9c4e37688aa0eb2e0ca3944989cd15b0c791e52
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 23:04:01 +0200

    lesson3: added home component

---
+++ todo_notes/frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1 className='text-center'>ToDo-Notes Project</h1>
+            <h3 className='text-center'>Click on the menu above to navigate</h3>
+        </div>
+    )
+}
+
+export default Home;
