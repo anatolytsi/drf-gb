027d4fcd7c7bfb3717452041bb333b19eea8a161
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 18:21:11 +0200

    lesson3: added url getter

--- todo_notes/frontend/src/App.js
+++ todo_notes/frontend/src/App.js
@@ -5,8 +5,8 @@ import axios from "axios";
 import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 
-const baseUrl = 'http://localhost:8000/api';
-const usersPath = '/users';
+const apiUrl = 'http://localhost:8000/api/';
+const getUrl = (name) => `${apiUrl}${name}`;
 
 class App extends React.Component {
     constructor(props) {
@@ -17,7 +17,7 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        axios.get(`${baseUrl}${usersPath}`)
+        axios.get(getUrl('users'))
             .then(response => {
                 const users = response.data;
                 this.setState(
