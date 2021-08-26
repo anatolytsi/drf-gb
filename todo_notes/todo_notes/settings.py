b131af41cf0b8a5dad386d002f6b4117c65f8df5
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-08-26 21:57:13 +0200

    lesson3: added camel case renderer and parser

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -148,7 +148,12 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ]
 }
