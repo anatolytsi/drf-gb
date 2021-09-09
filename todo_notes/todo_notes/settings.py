e49a2deba37d56e4cd011a88f26459f6c844435a
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-09-09 12:21:18 +0200

    lesson6: added permission classes and auth token

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'django_filters',
+    'rest_framework.authtoken',
 
     # custom apps
     'users',
@@ -160,4 +161,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissions'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
--- todo_notes/todo_notes/urls.py
+++ todo_notes/todo_notes/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from notes.views import NoteModelViewSet, ProjectModelViewSet
@@ -28,4 +29,5 @@ router.register('projects', ProjectModelViewSet)  # Register Project model view 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),  # These urls are now determined automatically by the router
+    path('api-token-auth/', views.obtain_auth_token),
 ]
