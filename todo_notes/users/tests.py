ca643946d56f5265213a71782dfbc0d11bb24f14
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-09-14 17:15:20 +0200

    lesson8: added user tests

--- todo_notes/users/tests.py
+++ todo_notes/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from users.models import User
+
+
+class TestUserViewSet(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user',
+                                             email='user@gb.local',
+                                             password='geekbrains')
+
+    def test_get_detail_no_auth(self):
+        client = APIClient()
+        response = client.get(f'/api/users/{self.user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_detail_auth(self):
+        client = APIClient()
+        client.force_authenticate(user=self.user)
+        response = client.get(f'/api/users/{self.user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
