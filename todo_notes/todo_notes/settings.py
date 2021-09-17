ff550fd84b272633f2d3c744bc6d98b811757c76
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-09-17 17:03:09 +0200

    lesson9: added new API version

--- todo_notes/todo_notes/settings.py
+++ todo_notes/todo_notes/settings.py
@@ -169,4 +169,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
 }
