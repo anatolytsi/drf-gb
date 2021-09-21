5b46f126b1a4f93ec9aab116a5c41d3eea512b38
Anatolii Tsirkunenko <tsirkunenkoat@gmail.com>
2021-09-21 21:34:05 +0200

    lesson10: added GraphQL schemes to get by id and strings

--- todo_notes/todo_notes/schema.py
+++ todo_notes/todo_notes/schema.py
@@ -1,4 +1,6 @@
 import graphene
+from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from graphene_django import DjangoObjectType
 
 from notes.models import Project, Note
@@ -25,17 +27,41 @@ class NoteType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     all_users = graphene.List(UserType)
+    user_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
+    users_by_name = graphene.List(UserType, name=graphene.String())
+    project_by_id = graphene.Field(ProjectType, id=graphene.Int(required=True))
+    projects_by_name = graphene.List(ProjectType, name=graphene.String())
+    note_by_id = graphene.Field(NoteType, id=graphene.Int(required=True))
+    notes_by_body = graphene.List(NoteType, body=graphene.String())
     all_projects = graphene.List(ProjectType)
     all_notes = graphene.List(NoteType)
 
     def resolve_all_users(self, info):
         return User.objects.all()
 
+    def resolve_user_by_id(self, info, id):
+        return get_object_or_404(User, pk=id)
+
+    def resolve_users_by_name(self, info, name=''):
+        return User.objects.filter(Q(first_name__contains=name) | Q(last_name__contains=name))
+
     def resolve_all_projects(self, info):
         return Project.objects.all()
 
+    def resolve_project_by_id(self, info, id):
+        return get_object_or_404(Project, pk=id)
+
+    def resolve_projects_by_name(self, info, name=''):
+        return Project.objects.filter(name__contains=name)
+
     def resolve_all_notes(self, info):
         return Note.objects.all()
+
+    def resolve_note_by_id(self, info, id):
+        return get_object_or_404(Note, pk=id)
+
+    def resolve_notes_by_body(self, info, body=''):
+        return Note.objects.filter(body__contains=body)
 
 
 schema = graphene.Schema(query=Query)
