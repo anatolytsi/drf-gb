58ff08ebb84f79c9400c99c62df605e564601be9
anatolytsi <snipl33t@gmail.com>
2021-09-28 22:44:09 +0200

    lesson12: switched database

--- production/todo_notes/todo_notes/settings.py
+++ production/todo_notes/todo_notes/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'todo_notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'todo_notes',
+        'USER': env('DEFAULT_SU_LOGIN'),
+        'PASSWORD': env('DEFAULT_SU_PASS'),
     }
 }
 
